01385f24eaf9eca90a9ec35d1719c8acbd31052b
silent-rider <silent.30.rider.10@gmail.com>
2025-09-17 17:47:56 +0700

    Removed useless Student class

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from vowels import count_vowels, check_age
+from vowels import count_vowels
 
 
 def main():
-    print(count_vowels("hello"))
-    print(count_vowels("Alabama"))
-    print(count_vowels("nut"))
-    print(count_vowels("rigid"))
-    print(count_vowels("baby"))
-    print(count_vowels("matter"))
-    print(count_vowels("onomatopoeia"))
-    check_age()
-    check_age()
-    check_age()
+    pass
 
 main()
