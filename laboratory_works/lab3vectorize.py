c241a1da0649f8a5794bd8f7b38d91c6a1c88d80
silent-rider <silent.30.rider.10@gmail.com>
2025-10-23 00:16:18 +0700

    Сделал 1-е задание второй лабы

--- laboratory_works/lab3vectorize.py
+++ laboratory_works/lab3vectorize.py
@@ -1,21 +1,34 @@
 import numpy as np
 
-def vectorize_sequence(data_vector, number_range = 300):
-    result_matrix = np.zeros((len(data_vector), number_range - 1))
-    for i, value in enumerate(data_vector):
-        result_matrix[i, value] = 1
-    return result_matrix
+
+def vector_to_binary_matrix(data_vector):
+    data = np.array(data_vector)
+    if data.min() < 0:
+        raise ValueError("Data must be positive")
+
+    max_val = data.max()
+    if max_val == 0:
+        num_bits = 1
+    else:
+        num_bits = int(np.floor(np.log2(max_val)) + 1)
+
+    powers = 2 ** np.arange(num_bits - 1, -1, -1)
+    binary_matrix = (data[:, None] & powers) > 0
+    return binary_matrix.astype(np.uint8)
 
 input_file = 'input_vector.npy'
 
-input_vector = np.random.rand(1000, 1) * 300 + 1
-input_vector = input_vector.astype('int8')
+power = 8
+#Создание входного вектора
+input_vector = np.random.rand(1000) * (2 ** power)
+input_vector = input_vector.astype('uint8')
 np.save(input_file, input_vector)
 
 input_vector = np.load(input_file, allow_pickle=True)
+print(input_vector[:5])
 
-output_matrix = vectorize_sequence(input_vector)
-print(output_matrix)
+output_matrix = vector_to_binary_matrix(input_vector, power + 1)
+print(output_matrix[:3])
 np.save('output_matrix.npy', output_matrix)
 
 
